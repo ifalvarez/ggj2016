c35a5e463dcb07d3f63bad3e9836857a1dc2c371
Ivan Alvarez <ivan.alvarez.r@gmail.com>
2016-01-31 01:22:23 -0500

    Added music tracks

---
+++ Assets/GGJ2016/Scripts/Bgm.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bgm : MonoBehaviour {
+
+    public AudioClip[] bgms;
+    private AudioSource audioSource;
+
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = bgms[Random.Range(0, bgms.Length)];
+        audioSource.Play();
+	}
+	
+	void Update () {
+	
+	}
+}
